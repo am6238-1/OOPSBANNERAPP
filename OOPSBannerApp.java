4985d434499fea53fc3d2fbc22977355541eab2b
Your Name <your@email.com>
2026-02-17 15:14:20 +0530

    Initial commit

---
+++ OOPSBannerApp.java
@@ -0,0 +1,13 @@
+/**
+ * OOPSBannerApp
+ * Use Case 1: Print OOPS to console
+ *
+ * @author Anushka
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
